0ec441e79c82bd69b8f84930e0627781ff3dd02c
Vidhey Oza <vidhey97@gmail.com>
2020-12-16 12:53:41 +0530

    Tweet class helper functions

--- Tweet.py
+++ Tweet.py
@@ -2,3 +2,6 @@ class Tweet(object):
     def __init__(self, content, polarity):
         self.content = content
         self.polarity = polarity
+
+    def __str__(self):
+        return "{} == {}".format(self.content, self.polarity)
